1e779eac53c5a307edab505571b43f358a0c2507
Sten Henriksson <you@example.com>
2025-05-24 23:52:29 +0200

    update keybinding

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,15 +12,16 @@ return {
         { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
         { "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
 
-        -- New keybindings for direct file navigation (h1-h9)
-        { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Harpoon file 1" },
-        { "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Harpoon file 2" },
-        { "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Harpoon file 3" },
-        { "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Harpoon file 4" },
-        { "<leader>h5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "Harpoon file 5" },
-        { "<leader>h6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", desc = "Harpoon file 6" },
-        { "<leader>h7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", desc = "Harpoon file 7" },
-        { "<leader>h8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", desc = "Harpoon file 8" },
-        { "<leader>h9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", desc = "Harpoon file 9" },
+        -- New keybindings for direct file navigation (1-9)
+        { "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Harpoon file 1" },
+        { "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Harpoon file 2" },
+        { "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Harpoon file 3" },
+        { "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Harpoon file 4" },
+        { "<leader>5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "Harpoon file 5" },
+        { "<leader>6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", desc = "Harpoon file 6" },
+        { "<leader>7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", desc = "Harpoon file 7" },
+        { "<leader>8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", desc = "Harpoon file 8" },
+        { "<leader>9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", desc = "Harpoon file 9" },
     },
 }
+
