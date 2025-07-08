9186a129c1c3e126315f70faa37deffcc4c60a47
Your Name <you@example.com>
2025-07-08 02:52:56 +0200

    chore: add empty line at end of options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -11,3 +11,4 @@ o.foldexpr = 'nvim_treesitter#foldexpr()'  -- Use treesitter for folding
 o.foldlevel = 99         -- Start with all folds open
 o.foldlevelstart = 99    -- Start with all folds open when opening files
 o.foldenable = true      -- Enable folding
+
